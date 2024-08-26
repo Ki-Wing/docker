1ff503240ca9db644eb116d727fa47e43212f4b0
ki-wing <dldpsk151@gmail.com>
2024-08-26 15:49:18 +0900

    feat: Add docker compose

--- basic1/HelloWorld2.java
+++ basic1/HelloWorld2.java
@@ -1,5 +1,7 @@
-public class HelloWorld1{
+public class HelloWorld2{
     public static void main(String[] args){
-        System.out.println("hihi");
+
+        System.out.println("hihi2");
+
     }
 }
